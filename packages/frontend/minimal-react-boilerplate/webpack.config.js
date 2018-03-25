b2b7aba7b0100846008fa1ff17a8da8d5dd93ca8
NEL\Martin <petit.martin125@gmail.com>
2018-03-25 17:25:48 +0200

    General: structure of the project Frontend: Webpack 4 + React + Babel initial setup

---
+++ packages/frontend/minimal-react-boilerplate/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['react-hot-loader', 'babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 3000
+  }
+}
