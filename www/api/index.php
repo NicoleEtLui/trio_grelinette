c7ead07628e8de9e0ca061c8809bae3c85020c0f
NEL\Martin <petit.martin125@gmail.com>
2018-04-21 10:57:22 +0200

    Moved config db.php to avoid it being on remote

--- www/api/index.php
+++ www/api/index.php
@@ -3,7 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
-require './src/config/db.php';
+require '../../db.php';
 
 mb_language('uni');
 mb_internal_encoding('UTF-8');
