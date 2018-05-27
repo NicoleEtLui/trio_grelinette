4885fcdfe808a176d7a6b1b16ad642f5e4f805c9
NEL\Martin <petit.martin125@gmail.com>
2018-05-27 11:26:56 +0200

    Backend-Routes: get rid of useless routes

--- www/api/src/routes/legumes.php
+++ www/api/src/routes/legumes.php
@@ -5,71 +5,12 @@ use \Psr\Http\Message\ResponseInterface as Response;
 // get all legumes
 
 $app->get('/legumes', function (Request $request, Response $response){
-    $query = "SELECT * FROM legumes";
-
-    // try {
-    //   // instance db class
-    //   $db = new db();
-
-    //   $db = $db->connect();
-
-    //   $stmt = $db->query($query);
-    //   $legumes = $stmt->fetchAll(PDO::FETCH_OBJ);
-    //   $db = null;
-    //   // print_r($legumes);
-
-    //   return $response->withJson($legumes, 200, JSON_UNESCAPED_UNICODE);
-    // } catch(PDOException $e) {
-    //   echo '{"error": {"text", '.$e->getMessage().'}}';
-    // }
     try {
-      $legumes = Legumes::find_many();
-      echo $legumes;
+      // return a set of array for each row instead of ORM classes instance
+      // with find_many
+      $legumes = Legumes::find_array();
+      return $response->withJson($legumes, 200, JSON_UNESCAPED_UNICODE);
     } catch(PDOException $e) {
       echo '{"error": {"text", '.$e->getMessage().'}}';
     }
-});
-
-// get a legumes by its id /api/public/legumes/1
-
-$app->get('/legumes/{id}', function (Request $request, Response $response, array $args) {
-  $id = $args['id'];
-  $query = "SELECT * from legumes WHERE leg_id=:id;";
-
-  try {
-    $db = new db();
-
-    $dbConnection = $db->connect();
-
-    $stmt = $dbConnection->prepare($query);
-    $stmt->execute(array(':id' => $id));
-    $legume = $stmt->fetchAll(PDO::FETCH_OBJ);
-    $db = null;
-
-    return $response->withJson($legume, 200, JSON_UNESCAPED_UNICODE);
-  } catch(PDOException $e) {
-    echo '{"error": {"text", '.$e->getMessage().'}}';
-  }
-});
-
-// // get a legumes by its label / api/public/legumes/label/tomate
-
-$app->get('/legumes/label/{label}', function (Request $request, Response $response, array $args) {
-  $label = $args['label'];
-  $query = "SELECT * from legumes WHERE label=:label;";
-
-  try {
-    $db = new db();
-
-    $dbConnection = $db->connect();
-
-    $stmt = $dbConnection->prepare($query);
-    $stmt->execute(array(':label' => $label));
-    $legumes = $stmt->fetchAll(PDO::FETCH_OBJ);
-    $db = null;
-
-    echo json_encode($legumes, JSON_UNESCAPED_UNICODE);
-  } catch(PDOException $e) {
-    echo '{"error": {"text", '.$e->getMessage().'}}';
-  }
 });
