69dbe264d5ff7fc9c2058b0e145037d168ae7021
NEL\Martin <petit.martin125@gmail.com>
2018-05-28 18:22:39 +0200

    General: post commande working

--- www/api/src/routes/commandes.php
+++ www/api/src/routes/commandes.php
@@ -3,12 +3,16 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app->post('/commandes/add', function(Request $request, Response $response) use ($database) {
+  $data = $request->getParsedBody();
+  // console_log($data);
   try {
     //parsedbody slim
     $database->insert("commandes", [
-      "cli_id" => "bidule@exemple.com",
+      "cli_id" => $data['commande']['cli_id'],
       "date_com" => "2018-05-27"
     ]);
+    //récupérer l'i de la commande pour le passer a comm_leg
+    //map over each array command
     $database->insert("comm_leg", [
       "com_id" => "8",
       "leg_id" => "1",
--- www/frontend/src/Container/CartForm.js
+++ www/frontend/src/Container/CartForm.js
@@ -7,6 +7,8 @@ import styled from 'styled-components'
 import Basket from '../../assets/svg/basket.svg'
 import { colors } from '../../style/variables'
 
+const moment = require('moment')
+
 const StyledBasket = styled(Basket)`
   fill: #fff;
   width: 28px;
@@ -23,17 +25,35 @@ const CartButton = styled.button`
   padding: 20px 15px;
   justify-content: space-between;
 `
-
 class CartForm extends React.Component {
   render () {
-    console.log('render')
+    let commande = {
+      'cli_id': '',
+      'date_com': moment().format('YYYY-MM-DD'),
+      'listeLegumes': this.props.legumes
+    }
     return (
       <Formik
+        initialValues={{
+          email: 'machin@example.com'
+        }}
         isSubmitting
-        onSubmit={() => this.props.postCommande()}
-        render={() => (
+        onSubmit={
+          values => {
+            commande.cli_id = values.email
+            this.props.postCommande(commande)
+          }
+        }
+        render={({
+          values,
+          handleChange
+        }) => (
           <Form>
-            <input type='email' name='email' />
+            <input
+              type='email'
+              name='email'
+              placeholder={values.email}
+              onChange={handleChange} />
             <CartButton type='submit'>
               Valider ma commande
               <StyledBasket />
@@ -45,13 +65,15 @@ class CartForm extends React.Component {
   }
 }
 
-export default connect(props => ({
+const ConnectedCartForm = connect(props => ({
   postCommande: commande => ({
     postCommandeResponse: {
       url: `http://192.168.99.100:8080/api/commandes/add`,
       method: 'POST',
       body: JSON.stringify({ commande }),
-      then: console.log('coucou')
+      then: console.log('post then')
     }
   })
 }))(CartForm)
+
+export default ConnectedCartForm
